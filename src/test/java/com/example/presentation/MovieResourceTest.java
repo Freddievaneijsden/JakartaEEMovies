f40f7b07626569d56470c33669a02ba6c320858b
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-03-06 14:28:43 +0100

    Update pom.xml with mockito

--- src/test/java/com/example/presentation/MovieResourceTest.java
+++ src/test/java/com/example/presentation/MovieResourceTest.java
@@ -3,20 +3,22 @@ package com.example.presentation;
 import com.example.business.MovieService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
+
+@ExtendWith(MockitoExtension.class)
 class MovieResourceTest {
 
+    @Mock
+    MovieService movieService;
 
     @Test
     @DisplayName("Get movie by Id returns movie with same id as paramter")
     void getMovieByIdReturnsMovieWithSameIdAsParamter() {
-//        MovieResource movieResource = new MovieResource(mock(movieService.class));
-//        var movie = movieResource.getOneMovie(1L);
-//        assertThat()
-
-
 
     }
 
