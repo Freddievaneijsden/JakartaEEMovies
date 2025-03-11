fcca6fe1479da2115819f9d36bb0990c3e96e9a4
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-03-11 16:43:02 +0100

    Add filtering methods in repository

--- src/main/java/com/example/persistence/MovieRepository.java
+++ src/main/java/com/example/persistence/MovieRepository.java
@@ -3,9 +3,15 @@ package com.example.persistence;
 import com.example.entity.Movie;
 import jakarta.data.repository.CrudRepository;
 import jakarta.data.repository.Repository;
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Long> {
     //Grundläggande CRUD finns via CrudRepository
     //Kan skriva egna om man följer regler för namngivning
+
+    List<Movie> findByMovieTitle(String title);
+    List<Movie> findByMovieDirector(String title);
+    List<Movie> findByMovieDurationGreaterThan(Integer duration);
+
 }
