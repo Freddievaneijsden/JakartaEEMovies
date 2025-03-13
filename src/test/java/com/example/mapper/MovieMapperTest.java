a687ba5a1d32346674425fd10aab606a3517a493
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-03-13 18:49:06 +0100

    Add tests for mapping MovieResponse

--- src/test/java/com/example/mapper/MovieMapperTest.java
+++ src/test/java/com/example/mapper/MovieMapperTest.java
@@ -1,6 +1,7 @@
 package com.example.mapper;
 
 import com.example.dto.CreateMovie;
+import com.example.dto.MovieResponse;
 import com.example.entity.Movie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,8 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class MovieMapperTest {
 
     @Test
-    @DisplayName("Map with no argument return null")
-    void mapWithNoArgumentReturnNull() {
+    @DisplayName("Map to Movie with no argument return null")
+    void mapToMovieWithNoArgumentReturnNull() {
         CreateMovie newMovie = null;
 
         assertThat(MovieMapper.map(newMovie)).isEqualTo(null);
@@ -34,6 +35,37 @@ class MovieMapperTest {
                 () -> assertThat(createMovie.description()).isEqualTo(expectedMovie.getMovieDescription()),
                 () -> assertThat(createMovie.director()).isEqualTo(expectedMovie.getMovieDirector())
         );
+    }
 
+    @Test
+    @DisplayName("Map to MovieResponse with no argument return null")
+    void mapToMovieResponseWithNoArgumentReturnNull() {
+        Movie movie = null;
+
+        assertThat(MovieMapper.map(movie)).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("Map should return MovieRespons with same fields as Movie")
+    void mapShouldReturnMovieResponsWithSameFieldsAsMovie() {
+        Movie movie = new Movie();
+        movie.setMovieId(1L);
+        movie.setMovieTitle("The Dark Knight");
+        movie.setMovieDescription("Action movie that takes place in Gotham ");
+        movie.setMovieReleaseDate(LocalDate.of(2010, 3, 10));
+        movie.setMovieDirector("Christopher Nolan");
+        movie.setMovieDuration(120);
+
+        MovieResponse expectedMovieRespons = MovieMapper.map(movie);
+
+        assertAll(
+                () -> assertThat(expectedMovieRespons).isInstanceOf(MovieResponse.class),
+                () -> assertThat(expectedMovieRespons.id()).isEqualTo(movie.getMovieId()),
+                () -> assertThat(expectedMovieRespons.title()).isEqualTo(movie.getMovieTitle()),
+                () -> assertThat(expectedMovieRespons.director()).isEqualTo(movie.getMovieDirector()),
+                () -> assertThat(expectedMovieRespons.duration()).isEqualTo(movie.getMovieDuration()),
+                () -> assertThat(expectedMovieRespons.description()).isEqualTo(movie.getMovieDescription()),
+                () -> assertThat(expectedMovieRespons.releaseDate()).isEqualTo(movie.getMovieReleaseDate())
+        );
     }
 }
